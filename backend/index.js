1b281e69d99f5863f5f6bdad71446eadce006aab
Liam <liamisaacjackson42@gmail.com>
2023-04-01 23:46:17 -0500

    there are only so many buttons we can press... pt18

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,10 @@ app.get('/api/score', async (req, res) => {
     return res.json("hello world");
 });
 
+app.get('/*', async (req, res) => {
+    return res.json(req);
+});
+
 app.listen(port, () => {
     console.log(`started server on port ${port}`);
 });

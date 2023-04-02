e2b76618cb1a884354399785031d69eca3bc5125
Liam <liamisaacjackson42@gmail.com>
2023-04-01 22:32:47 -0500

    there are only so many buttons we can press...

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-// import path, { dirname } from 'path';
-// import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 5000;
-// const DIR = fileURLToPath(dirname(import.meta.url));
+const DIR = fileURLToPath(dirname(import.meta.url));
 
-// app.use('/', express.static('frontend'));
+app.use('/', express.static('frontend'));
 
 app.get('/score', async (req, res) => {
     return res.json("hello world");

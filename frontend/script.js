051f9c226502d6c3786ca687aef9c417f48fa254
Liam <liamisaacjackson42@gmail.com>
2023-04-10 11:45:01 -0500

    sort the right way, fix time spacing on leaderboard

--- frontend/script.js
+++ frontend/script.js
@@ -466,7 +466,7 @@ class DBHandler {
             .then(records => {
                 DBHandler.leaderboards[lvl_id] = records;
                 console.log(DBHandler.leaderboards[lvl_id]);
-                DBHandler.leaderboards[lvl_id].sort((a, b)=>{a.time - b.time})
+                DBHandler.leaderboards[lvl_id].sort((a, b)=>{b.time - a.time})
                 DBHandler.waiting_for_updates--;
             });
     }
@@ -523,7 +523,7 @@ class Leaderboard {
         ctx.fillStyle = "white";
         ctx.font = `${record_fontsize}px Helvetica`;
         ctx.fillText(name, x+10, y+record_fontsize+8);
-        ctx.fillText(time_txt, x+width-10-record_fontsize/3*time_txt.length, y+record_fontsize+10);
+        ctx.fillText(time_txt, x+width-10-record_fontsize/2*time_txt.length, y+record_fontsize+10);
     }
 }
 
